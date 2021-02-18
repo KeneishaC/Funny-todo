0fefa5a2818f33027834f21df4eae753b7753400
keneishac <keneishacorbett@gmail.com>
2021-02-18 15:30:50 -0800

    adds update to edit input and button

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 function TodoForm( props) {
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState(props.edit ? props.edit.value : '')
 
     const inputRef = useRef(null)
 
@@ -26,7 +26,22 @@ function TodoForm( props) {
 
     return (
         <form className='todo-form' onSubmit={handleSubmit}> 
+            {props.edit ? (
+            <>
             <input 
+                type='text' 
+                placeholder='Update your todo' 
+                value={input} 
+                name='text' 
+                className='todo-input'
+                onChange={handleChange}
+                ref={inputRef}
+            />
+            <button className='todo-button'>Update</button>
+             </>
+            ) : (
+                <>
+                <input 
                 type='text' 
                 placeholder='Add a funny todo' 
                 value={input} 
@@ -36,6 +51,9 @@ function TodoForm( props) {
                 ref={inputRef}
             />
             <button className='todo-button'> Add funny todo</button>
+            </>
+            )}
+           
         </form>
     )
 }
