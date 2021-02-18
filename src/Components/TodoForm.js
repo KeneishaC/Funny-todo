f8d34d76bd7bb8ecc07572fb72cda3c2580f2f3b
keneishac <keneishacorbett@gmail.com>
2021-02-18 13:27:13 -0800

    useres are able to type in form input

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -1,17 +1,31 @@
 import React, { useState } from 'react'
 
-function TodoForm() {
+function TodoForm( props) {
     const [input, setInput] = useState('')
 
+    const handleChange = (e) => {
+        setInput(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        props.onsubmit({
+            //making the chances of getting the same id unlikely
+            id: Math.floor(Math.random() * 1000),
+            text: input 
+        })
+    }
 
     return (
-        <form className='todo-form'> 
+        <form className='todo-form' onSubmit={handleSubmit}> 
             <input 
                 type='text' 
                 placeholder='Add a funny todo' 
                 value={input} 
                 name='text' 
                 className='todo'
+                onChange={handleChange}
             />
             <button className='todo-button'> Add funny todo</button>
         </form>
